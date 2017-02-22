f2d15824317d4abdd5bf83381050ee540b127939
vestalux <istvan.lechner@sufni365.hu>
2017-02-22 20:48:05 +0100

    Update all.js

--- js/all.js
+++ js/all.js
@@ -1,3 +1,12 @@
 ---
 ---
 {% include_relative main.js %}
+{% include_relative vendor/jquery-1.10.2.min.js %}
+{% include_relative bootstrap.min.js %}
+{% include_relative owl.carousel.min.js %}
+{% include_relative min/plugins.js %}
+{% include_relative min/waypoints.min.js %}
+{% include_relative jquery.counterup.js%}
+
+
+
